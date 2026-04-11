a156db8767ccb1941582dedb2474922180d4693f
jackra1n <45038833+jackra1n@users.noreply.github.com>
2026-04-11 14:36:02 +0200

    style: remove whitespace

--- ZumoTemplate/ZumoApp/Program.cs
+++ ZumoTemplate/ZumoApp/Program.cs
@@ -1,10 +1,10 @@
-﻿//    _____                            ____        __          __
+//    _____                            ____        __          __
 //   /__  /  __  ______ ___  ____     / __ \____  / /_  ____  / /_
 //     / /  / / / / __ `__ \/ __ \   / /_/ / __ \/ __ \/ __ \/ __/
 //    / /__/ /_/ / / / / / / /_/ /  / _, _/ /_/ / /_/ / /_/ / /_
 //   /____/\__,_/_/ /_/ /_/\____/  /_/ |_|\____/_.___/\____/\__/
 //   (c) Hochschule Luzern T&A ========== www.hslu.ch ============
-//   
+//
 using ZumoLib;
 
 namespace ZumoApp;
@@ -14,7 +14,7 @@ class Program
     static void Main(string[] args)
     {
         Utils.WaitForDebugger();
-        
+
         Zumo.Instance.Cm4Button.ButtonChanged += ButtonChanged;
         Zumo.Instance.ZumoButton.ButtonChanged += ButtonChanged2;
 
